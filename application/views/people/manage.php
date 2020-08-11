abc8a93aff82678e61b1ae95450890cd0b6c92bd
thyagocarlos <thiago-carlos@hotmail.com>
2020-08-11 13:06:16 -0300

    stylize buttoms

--- application/views/people/manage.php
+++ application/views/people/manage.php
@@ -60,12 +60,12 @@ function post_person_form_submit(response)
 	<div id="title" class="float_left"><?php echo $this->lang->line('common_list_of').' '.$this->lang->line('module_'.$controller_name); ?></div>
 	<div id="new_button">
 		<?php echo anchor("$controller_name/view/-1/width:$form_width",
-		"<div class='big_button' style='float: left;'><span>".$this->lang->line($controller_name.'_new')."</span></div>",
+		"<div class='btn btn-primary' style='float: left;'><span>".$this->lang->line($controller_name.'_new')."</span></div>",
 		array('class'=>'thickbox none','title'=>$this->lang->line($controller_name.'_new')));
 		?>
 		<?php if ($controller_name =='customers') {?>
 			<?php echo anchor("$controller_name/excel_import/width:$form_width",
-			"<div class='big_button' style='float: left;'><span>".$this->lang->line('common_excel_import')."</span></div>",
+			"<div class='ml-2 btn btn-success' style='float: left;'><span>".$this->lang->line('common_excel_import')."</span></div>",
 				array('class'=>'thickbox none','title'=>'Import Items from Excel'));
 			?>	
 		<?php } ?>
